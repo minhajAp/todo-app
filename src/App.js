6c72c2b7bd75f11b2bdc5143c032a0eef50cd349
minhaj <jinumonap71@gmail.com>
2022-01-13 13:49:07 +0530

    get data from local storage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import logo from "./logo.svg"
 import "./App.css"
 
 function App() {
+  const add_todo = () => {
+    const todo =
+      document.getElementById("todo").value
+    if (todo) {
+      // Save
+      const get_datas = localStorage["datas"]
+      console.log(get_datas)
+      //   todo_string=JSON.stringify(todo)
+      //  get_datas.push(todo)
+      localStorage["datas"] = [todo, get_datas]
+      alert(todo)
+    }
+    // Retrieve
+  }
+
+  const stored_datas =
+    localStorage["datas"].split(",")
+  // console.log(split_string)
+  // const stored_datas = localStorage["datas"]
+  // console.log(stored_datas)
+
   return (
     <div>
       <div className="w-screen flex justify-center bg-green-500 h-screen">
-        <div className="bg-green-800 w-4/6 max-w-xs mt-10 mb-10 rounded-lg">
+        <div className="bg-green-800 w-4/6 max-w-xs mt-10 mb-10 rounded-lg  min-h-[350px]">
           <h1 className="text-center font-extrabold m-2 underline">
             TO DO APP
           </h1>
@@ -13,16 +33,20 @@ function App() {
             <input
               className="w-11/12 p-2 rounded-md"
               type="text"
+              id="todo"
             />
             <div className="w-11/12 flex justify-center">
-              <button className="bg-green-500 w-7/12 font-semibold border-4 mt-2 rounded-lg">
+              <button
+                onClick={add_todo}
+                className="bg-green-500 w-7/12 font-semibold border-4 mt-2 rounded-lg"
+              >
                 Add
               </button>
             </div>
             <div className="mt-4 ml-3">
-              <li className="font-boldpb-2">
-                propose her😘
-              </li>
+              {stored_datas.map(user => (
+                <li key={user} >{user}</li>
+              ))}
             </div>
           </div>
         </div>
@@ -30,5 +54,4 @@ function App() {
     </div>
   )
 }
-
 export default App
